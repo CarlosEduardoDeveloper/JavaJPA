ab3d9a97cf6a60a0640ea31eeb3fcb53fd894363
carlos.eduardo <https://github.com/CarlosEduardoDeveloper/BVAtividades.git>
2021-08-19 08:38:18 -0300

    Projeto JPA

---
+++ src/main/java/br/com/alura/loja/testes/CadastroDeProduto.java
@@ -0,0 +1,30 @@
+package br.com.alura.loja.testes;
+
+import java.math.BigDecimal;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+import br.com.alura.loja.modelo.Produto;
+
+public class CadastroDeProduto {
+	
+	public static void main(String[] args) {
+		Produto celular = new Produto();
+		celular.setNome("Xiaomi REDMI");
+		celular.setDescricao("Muito legal");
+		celular.setPreco(new BigDecimal("800"));
+		
+		EntityManager em = JPAUtil.getEntityManager();
+		ProdutoDao dao = new ProdutoDao(em);	
+		
+		em.getTransaction().begin();
+		em.persist(celular);
+		em.getTransaction().commit();
+		em.close();
+		
+		
+	}
+
+}

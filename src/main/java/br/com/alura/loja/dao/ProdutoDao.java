b5dd283309cae485262fcc6c518f40e722017583
carlos.eduardo <https://github.com/CarlosEduardoDeveloper/BVAtividades.git>
2021-08-19 10:03:11 -0300

    Jpa2.0

--- src/main/java/br/com/alura/loja/dao/ProdutoDao.java
+++ src/main/java/br/com/alura/loja/dao/ProdutoDao.java
@@ -2,16 +2,18 @@ package br.com.alura.loja.dao;
 
 import javax.persistence.EntityManager;
 
+import br.com.alura.loja.modelo.Produto;
+
 public class ProdutoDao {
-	
-	private EntityManager em;
-		
-	public ProdutoDao(EntityManager em) {
-		this.em = em;
-	}
-	
-	public void cadastrar(Produto produtos) {
-		this.em.persist(produtos);
-	}	
-	
+
+    private EntityManager em;
+
+    public ProdutoDao(EntityManager em) {
+        this.em = em;
+    }
+
+    public void cadastrar(Produto produto) {
+        this.em.persist(produto);
+    }
+
 }
--- src/main/java/br/com/alura/loja/testes/CadastroDeProduto.java
+++ src/main/java/br/com/alura/loja/testes/CadastroDeProduto.java
@@ -3,24 +3,28 @@ package br.com.alura.loja.testes;
 import java.math.BigDecimal;
 
 import javax.persistence.EntityManager;
-import javax.persistence.EntityManagerFactory;
-import javax.persistence.Persistence;
 
+import br.com.alura.loja.dao.CategoriaDao;
+import br.com.alura.loja.dao.ProdutoDao;
+import br.com.alura.loja.modelo.Categoria;
 import br.com.alura.loja.modelo.Produto;
+import br.com.alura.loja.util.JPAUtil;
 
 public class CadastroDeProduto {
 	
 	public static void main(String[] args) {
-		Produto celular = new Produto();
-		celular.setNome("Xiaomi REDMI");
-		celular.setDescricao("Muito legal");
-		celular.setPreco(new BigDecimal("800"));
-		
+		Categoria celulares = new Categoria("CELULARES");
+		Produto celular = new Produto("Xiaomi REDMI", "Muito legal", new BigDecimal ("800"), celulares);
+				
 		EntityManager em = JPAUtil.getEntityManager();
-		ProdutoDao dao = new ProdutoDao(em);	
+		ProdutoDao ProdutoDao = new ProdutoDao(em);	
+		CategoriaDao CategoriaDao = new CategoriaDao(em);	
 		
 		em.getTransaction().begin();
-		em.persist(celular);
+		
+		CategoriaDao.cadastrar(celulares);
+		ProdutoDao.cadastrar(celular);
+		
 		em.getTransaction().commit();
 		em.close();
 		
